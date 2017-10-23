93ff906e9c9780109debf685206f85bc82ad5ae1
LEONARDO EDELMAN WAJNSZTOK <e1312737@arpoador.grad.inf.puc-rio.br>
2017-10-23 18:52:51 -0200

    main

--- Trabalho_3/src/Main.java
+++ Trabalho_3/src/Main.java
@@ -11,37 +11,66 @@ import java.security.cert.X509Certificate;
 
 public class Main {
 	public static void main (String[] args) throws ClassNotFoundException, SQLException {
-		new LoginView();
+//		new LoginView();
+		
+		String pastaKeys = "/home/ecp/e1312737/e1312737.pds.V2/Documents/INF1416/Trabalho_3/Pacote-T3/Keys/";
+		String pastaArquivos = "/home/ecp/e1312737/e1312737.pds.V2/Documents/INF1416/Trabalho_3/Pacote-T3/Files/";
+		String email = "user01@inf1416.puc-rio.br";
+		String senha = "321456";
+		String grupo = "usuario";
+		String fraseSecreta = "user01";
+		String pathChavePrivada = pastaKeys + "user01-pkcs8-pem-des.key";
+		String pathCertificado = pastaKeys + "user01-x509.crt";
 		
 		
-//		String pastaKeys = "/Users/LeoWajnsztok/Documents/PUC/INF1416/Trabalho_3/Pacote-T3/Keys/";
-//		String pastaArquivos = "/Users/LeoWajnsztok/Documents/PUC/INF1416/Trabalho_3/Pacote-T3/Files/";
-//		
-//		DBManager.insereRegistro(1001);
-////		Auth.cadastraUsuario("usuario", "321456", pastaKeys + "user01-x509.crt");
-//		HashMap user = Auth.autenticaEmail("user01@inf1416.puc-rio.br");
-//		System.out.println(user);
-//		
-//		PrivateKey chavePrivada = Auth.leChavePrivada("user01", pastaKeys + "user01-pkcs8-pem-des.key");
-//		System.out.println(Auth.testaChavePrivada(chavePrivada, (String) user.get("certificado"), user));
-//		
-//		String index = null;
-//		try {
-//			index = new String(Auth.decriptaArquivo(user, pastaArquivos, "index", chavePrivada), "UTF8");
-//		} catch (UnsupportedEncodingException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		System.out.println(index);
-//		
-//		System.out.println(Auth.acessarArquivo(user, index, "teste01.docx", chavePrivada, pastaArquivos));
-//		
-//		System.out.println(Auth.geraTanList("/Users/LeoWajnsztok/Documents/PUC/INF1416/Trabalho_3/", 10, "user01@inf1416.puc-rio.br"));
-//		
-//		
-////		System.out.println(db.retornaTanList("user01@inf1416.puc-rio.br"));
-////		DBManager.marcaTanUsada("user01@inf1416.puc-rio.br", 1);
-////		DBManager.marcaTanUsada("user01@inf1416.puc-rio.br", 2);
-////		System.out.println(DBManager.retornaTanList("user01@inf1416.puc-rio.br"));
+		DBManager.insereRegistro(1001);
+		Auth.cadastraUsuario(grupo, senha, pathCertificado);
+		HashMap user = Auth.autenticaEmail(email);
+		if (user == null) {
+			System.out.println("Email não autenticado");
+		}
+		else {
+			System.out.println("Email autenticado");
+		}
+		
+		if (Auth.autenticaSenha(senha, user) == false) {
+			System.out.println("Senha incorreta");
+		}
+		else {
+			System.out.println("Senha correta");
+			System.out.println(user);
+			
+			PrivateKey chavePrivada = Auth.leChavePrivada(fraseSecreta, pathChavePrivada);
+			System.out.println("Chave privada:");
+			System.out.println(chavePrivada);
+			System.out.println("Teste da chave privada: ");
+			
+			if (Auth.testaChavePrivada(chavePrivada, (String) user.get("certificado"), user)) {
+				System.out.println("Chave privada ok");
+			}
+			else {
+				System.out.println("Chave privada errada");
+			}
+			
+			String index = null;
+			try {
+				index = new String(Auth.decriptaArquivo(user, pastaArquivos, "index", chavePrivada), "UTF8");
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(index);
+			
+			System.out.println(Auth.acessarArquivo(user, index, "teste01.docx", chavePrivada, pastaArquivos));
+			
+			System.out.println(Auth.geraTanList("../", 10, email));
+			
+			
+			System.out.println(DBManager.retornaTanList("user01@inf1416.puc-rio.br"));
+			DBManager.marcaTanUsada("user01@inf1416.puc-rio.br", 1);
+			DBManager.marcaTanUsada("user01@inf1416.puc-rio.br", 2);
+			System.out.println(DBManager.retornaTanList("user01@inf1416.puc-rio.br"));
+		}
+		
 	}
 }
